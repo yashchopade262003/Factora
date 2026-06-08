e25bc6ebdf495dab87715703bbd73ec556f6fb55
yashc <yashc@192.168.1.34>
2026-06-08 23:31:34 +0530

    Initial Setup

---
+++ factora/inventry-service/src/main/java/org/inventry/service/InventryServiceApplication.java
@@ -0,0 +1,12 @@
+package org.inventry.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventryServiceApplication.class, args);
+	}
+}
